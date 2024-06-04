34e0c4ab435d8d9acb350f78fbaa6f892965826b
KD Preap <kd@KDs-MacBook-Pro.local>
2024-06-04 15:55:17 -0400

    updated

--- wikiapp/__init__.py
+++ wikiapp/__init__.py
@@ -2,8 +2,8 @@ import importlib.metadata
 import warnings
 
 try:
-    __version__ = importlib.metadata.version("wikiapp_rd")
+    __version__ = importlib.metadata.version("wikiapp_kd")
 except importlib.metadata.PackageNotFoundError as e:
-    warnings.warn("Could not determine version of wikiapp_rd", stacklevel=1)
+    warnings.warn("Could not determine version of wikiapp_kd", stacklevel=1)
     warnings.warn(str(e), stacklevel=1)
     __version__ = "unknown"
